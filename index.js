3f7b2bd844aeeec167a43ecdc1b382e2da47bbee
Lakshmi K Sathyan <78313062+lakki0704@users.noreply.github.com>
2021-11-28 23:48:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function markdone() {
+
+    //document.write("Your response is submitted ");
+    alert('Press back button instead of return ');
+}
+
+function mysearch() {
+    var x = document.getElementByClassName("search");
+    var input = document.getElementById("searchbar");
+    var in_value = input.value.toLowerCase();
+    var i;
+    for (i = 0; i < x.length; i++) {
+
+    }
+}
